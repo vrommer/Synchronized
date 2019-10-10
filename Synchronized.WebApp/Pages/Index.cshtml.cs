bd52d536e7106700a11a328a122c8ae89a407282
i306534 <vadim.rommer@sap.com>
2019-10-10 13:42:21 +0300

    Move notifications to details page

--- Synchronized.WebApp/Pages/Index.cshtml.cs
+++ Synchronized.WebApp/Pages/Index.cshtml.cs
@@ -5,8 +5,6 @@ using Synchronized.ViewModel.QuestionsViewModels;
 using Synchronized.ViewServices.Interfaces;
 using Synchronized.WebApp.Conventions;
 using System.Net.Http;
-using System;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Synchronized.WebApp.Pages
@@ -18,41 +16,22 @@ namespace Synchronized.WebApp.Pages
         public int CurrentPage { get; set; }
         public string SearchString { get; set; }
         public string SortOrder { get; set; }
-        public HttpClient Client { get; set; }
 
         private readonly ILogger<IndexModel> _logger;
         private readonly IQuestionsService _questionsService;        
 
         public IndexModel(
             ILogger<IndexModel> logger,
-            IQuestionsService localService,
-            HttpClient client
+            IQuestionsService localService
             )
         {
             _questionsService = localService;
             _logger = logger;
-            Client = client;
             CurrentPage = 1;
         }
 
         public async Task OnGetAsync([MustBeInQueryParameterConvention]int? pageNumber, [MustBeInQueryParameterConvention]string sortOrder = null)
-        {         
-            HttpRequestMessage httpRequest = new HttpRequestMessage(HttpMethod.Post, @"http://174.138.105.248/notifications");
-            httpRequest.Content = new StringContent("{" +
-                "\"recepients\":[\"vadim.rommer@gmail.com\"]," +
-                "\"subject\":\"Synchronized home page\"," +
-                "\"message\":\"Someone viewd synchronized homepage\"}",
-                                                Encoding.UTF8,
-                                                "application/json");
-
-            try {
-                var result = await Client.SendAsync(httpRequest);
-            } catch (Exception ex)
-            {
-                _logger.LogDebug("Notification not sent!");
-                _logger.LogDebug(ex.Message);
-            }
-            
+        {                                 
             CurrentPage = pageNumber ?? 1;
             Questions = await _questionsService.GetHomePageModel(CurrentPage);
         }
--- Synchronized.WebApp/Pages/Questions/Details.cshtml.cs
+++ Synchronized.WebApp/Pages/Questions/Details.cshtml.cs
@@ -6,6 +6,9 @@ using Synchronized.UI.Utilities;
 using Synchronized.ViewModel;
 using Synchronized.ViewModel.QuestionsViewModels;
 using Synchronized.ViewServices.Interfaces;
+using System;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Synchronized.WebApp.Pages.Questions
@@ -14,6 +17,7 @@ namespace Synchronized.WebApp.Pages.Questions
     {
         public QuestionForDetailsPage Question { get; set; }
         public string UserId { get; set; }
+        public HttpClient Client { get; set; }
 
         private readonly ILogger<DetailsModel> _logger;
         IQuestionsService _questionsService;
@@ -22,16 +26,35 @@ namespace Synchronized.WebApp.Pages.Questions
         public DetailsModel(
             IQuestionsService questionsService,
             ILogger<DetailsModel> logger,
-            UserManager<Domain.ApplicationUser> userManager
+            UserManager<Domain.ApplicationUser> userManager,
+            HttpClient client
         )
         {
             _questionsService = questionsService;
             _logger = logger;
             _userManager = userManager;
+            Client = client;
         }
 
         public async Task OnGetAsync(string id)
         {
+            HttpRequestMessage httpRequest = new HttpRequestMessage(HttpMethod.Post, @"http://174.138.105.248/notifications");
+            httpRequest.Content = new StringContent("{" +
+                "\"recepients\":[\"vadim.rommer@gmail.com\"]," +
+                "\"subject\":\"Synchronized home page\"," +
+                "\"message\":\"Someone viewd synchronized details page for post: " + id + "\"}",
+                                                Encoding.UTF8,
+                                                "application/json");
+
+            try
+            {
+                var result = await Client.SendAsync(httpRequest);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogDebug("Notification not sent!");
+                _logger.LogDebug(ex.Message);
+            }
             UserId = await Utils.GetUserIdAsync(HttpContext, _userManager);
             Question = await _questionsService.GetQuestionDetailsPageModel(id, UserId);
         }
@@ -40,7 +63,7 @@ namespace Synchronized.WebApp.Pages.Questions
         public AnswerViewModel Answer { get; set; }
 
         public async Task<IActionResult> OnPostAsync(string id)
-        {
+        {            
             if (!ModelState.IsValid)
             {
                 return Page();
